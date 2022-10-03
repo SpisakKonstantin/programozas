03e53221ce039b11a29128493f6027c90a8396fb
pa-spisakrk <91116211+SpisakKonstantin@users.noreply.github.com>
2022-10-03 09:23:17 +0200

    Add files via upload

---
+++ 9-10ora.py
@@ -0,0 +1,10 @@
+# első feladat 9tk
+idei_év = 2022
+print(type(idei_év))
+print('idei_év: ', idei_év, sep="--->")
+felhasznalo_kora= input('hány éves vagy?')
+print('te most', felhasznalo_kora, 'éves vagy.')
+print(' felhasználo_kora változo ',type(felhasznalo_kora))
+felhasznalo_kora = int(felhasznalo_kora)
+születesi_év = idei_év - felhasznalo_kora
+print('ekkor születtél: ', születesi_év, '.', sep="")

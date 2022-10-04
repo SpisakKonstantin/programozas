9b06d10af40eead2d0fbaaf9921e79b05696412c
pa-spisakrk <91116211+SpisakKonstantin@users.noreply.github.com>
2022-10-04 14:24:19 +0200

    Add files via upload

---
+++ 9-10hf.py
@@ -0,0 +1,9 @@
+a=int(input("kérek egy számot: " ))
+b=int(input("kérek még egy számot:  "))
+c=input("adjon meg egy műveletett:   ")
+if c=="-":
+    print(a-b)
+elif c=="+":
+    print(a+b)
+else:
+    ("Nem megfekekő paraméter")

08fd3811113fbee1e691cea5324dc275363be6eb
pa-spisakrk <91116211+SpisakKonstantin@users.noreply.github.com>
2022-10-24 09:41:43 +0200

    Add files via upload

---
+++ 2022.10.24 2.py
@@ -0,0 +1,55 @@
+"""
+szám = 0
+for x in "banana":
+    print(szám,x,sep="===",end='\n')
+
+    
+    szám = szám +1
+
+
+a = "hello, word "
+
+print(len(a))
+
+txt = "The best things in life are free!"
+
+
+if "freeee" in txt:
+    print("yes, 'free' is present.")
+else:
+    print("no, 'free' is present.")
+
+txt = "The best things in life are free!"
+
+print("expensive" not in txt)
+
+
+b = "Hello, World!"
+
+print(b[1:5])  # llo
+
+c = "H"+b[1:5]
+
+
+print(c)
+
+b = "Hello, World!"
+
+print(b[-5:-2]) # orl
+
+
+a = "Hello, World!"
+
+print(a.upper())
+
+
+a = "Hello, World!"
+  
+
+print(a.lower())
+"""
+a = " Hello, World! "
+print(a)
+print(a.strip()) # returns "Hello, World!"
+szám = 10
+szám.

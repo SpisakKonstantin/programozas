6f4ec06ab070860eb744ed5d154122217caed39d
pa-spisakrk <91116211+SpisakKonstantin@users.noreply.github.com>
2022-11-27 16:19:45 +0100

    Add files via upload

---
+++ 2022.11.21hf.py
@@ -0,0 +1,17 @@
+"""
+for szam1 in range (1,101):
+    print(szam1)
+
+for szam1 in range (1,101,3):
+    print(szam1)
+
+for szam1 in range (0,100,2):
+    print(szam1)
+
+for szam1 in range (1,100,2):
+    print(szam1)
+
+"""
+szöveg = ["házi feladat"]
+for szöveg in szöveg :
+  print(szöveg)

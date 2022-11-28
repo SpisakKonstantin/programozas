7068a49ec51204ba8a380795775ce8cb976df01d
pa-spisakrk <91116211+SpisakKonstantin@users.noreply.github.com>
2022-11-28 08:37:39 +0100

    Add files via upload

---
+++ 2022.11.28.py
@@ -0,0 +1,41 @@
+"""
+print("1.verzio")
+szam = 0
+for _ in range(100):
+    szam = szam +1 
+    print(szam,end=" ")
+    
+
+print("2.verzio")
+szam = 1
+for _ in range(100):
+    print(szam,end=" ")
+    szam = szam +1
+print()
+
+
+print("3.verzio")
+szam = 0
+for _ in range(1,100):
+    szam = szam +1
+    print(szam,end=" ")
+print()
+
+print("4.verzio")
+szam=0
+for _ in range(1,100,1):
+    szam = szam +1
+    print(szam,end=" ")
+print()
+
+print("5.verzio")
+szam = 0 
+for _ in range(5,105,1):
+    szam = szam +1 
+    print(szam,end=" ")
+print()
+"""
+print("6.verzio")
+for szam in range (100):
+    print(szam+1,end=" ")
+    print()

85f1e8cffdcf9730a462ff8ff3f5cdb9381c5ca8
pa-spisakrk <91116211+SpisakKonstantin@users.noreply.github.com>
2022-12-05 09:27:30 +0100

    Add files via upload

---
+++ 2022.12.05.py
@@ -0,0 +1,58 @@
+#A Pyhton   Lista
+lista = ["alma","banán","cseresznye"]
+print(lista)
+
+lista = ["alma","banán","cseresznye","alma","banán"]
+print(lista)
+print(lista[0])
+print(lista[3])
+
+lista = ["alma","banán","cseresznye"]
+print(len(lista))
+hossza = len(lista)
+print(hossza)
+
+lista1 = ["alma","banán","cseresznye"]
+lista2 = [1, 5, 7, 9, 3]
+lista3 =[True, False, False]
+
+lista1 = ["abc" , 34, True, 40, "férfi"]
+print(lista1)
+
+lista5 = ["alma","banán","cseresznye"]
+print(type(lista5))
+
+lista = list(("alma", "banán","cseresznye"))
+print(lista)
+
+lista = ["alma","banán","cseresznye"]
+print(lista[-1])
+print(lista[2])
+#cseresznye 
+
+lista = ["alma-0","banán-1","cseresznye-2", "narancs-3", "kivi-4", "szőlő-5", "mango-6"]
+print(lista[2:5])
+
+lista = ["alma-0","banán-1","cseresznye-2", "narancs-3", "kivi-4", "szőlő-5", "mango-6"]
+print(lista[2:])
+ujlista=lista[2:]
+print(ujlista)
+#lista_10d_1csop=[]
+
+lista_10d_1csop=["fb","gm","hk","kg","mm","oa","pj","pp","sp","sk","sm","tb","ts","tt"]
+csop1=(lista_10d_1csop[0:5])
+print(csop1)
+
+csop2=(lista_10d_1csop[5:10])
+print(csop2)
+
+csop3=(lista_10d_1csop[10:14])
+print(csop3)
+
+lista = ["alma-0","banán-1","cseresznye-2", "narancs-3", "kivi-4", "szőlő-5", "mango-6"]
+print(lista[-4:-1])
+
+lista_10d_1csop=["fb","gm","hk","kg","mm","oa","pj","pp","sp","sk","sm","tb","ts","tt"]
+ujlista=nemmegbukotak=["fb","gm","hk","kg","mm","oa","pj","pp","sp","sk"]
+print(ujlista)
+

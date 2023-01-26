cecf9975d2a445c0d25d67c31a4da28d59e144b9
pa-spisakrk <91116211+SpisakKonstantin@users.noreply.github.com>
2023-01-26 08:42:47 +0100

    Add files via upload

---
+++ 2023.01.26.py
@@ -0,0 +1,23 @@
+"""
+def menut_kiir(tipus):
+    if tipus == 2:
+        print('1. Új adat bevitele')
+        print('2. kilépés a programból')
+    if tipus == 3:
+        print('1. Új adat bevitele')
+        print('2. Adatok módosítása / törlése')
+        print('3. Kilépés a programból')
+
+
+#eljárás hívása 
+menut_kiir(3)
+"""
+def koszont():
+    print('üdv a fedélzeten!')
+
+def koszont_nevvel(nev):
+    print('Szia '+ nev + ',üdv a fedélzeten!')
+
+def koszont_nevvel2(nev):
+    print(f"{nev} udv a fedélzeten!")
+koszont_nevvel2()
